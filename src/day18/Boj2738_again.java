9d1677dbbc27444422d6374f729a8f1c406349c4
suchan5 <suchan5@hotmail.com>
2024-06-20 11:43:01 +0900

    Practise basic java algorithm Day18

---
+++ src/day18/Boj2738_again.java
@@ -0,0 +1,24 @@
+package day18;
+
+import java.util.Scanner;
+
+public class Boj2738_again {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] A = new int[N][M];
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                A[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                A[i][j] += scanner.nextInt(); // 두번째 array 받을때는 더해준다(출력값이 원하는게 더하는거임)
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
